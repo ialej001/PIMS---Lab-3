f10707646631ac388fe13a80aa1451d6c6802f24
bit-flipping-alejo <44530684+bit-flipping-alejo@users.noreply.github.com>
2018-10-27 11:06:22 -0700

    Alejo update #3
    Final one, im sorry for so many small ones. Just expanded variable names really

--- lab3.py
+++ lab3.py
@@ -42,18 +42,23 @@ def halfRed():
 
 
 # Problem 2
-def moreRed():
+#Potential issues: if your new red value
+# is greater than max red val as dictated
+# by pixel depth thats an issue. As such
+# this fcn puts a cieling at 255
+# assuming 8 bit pixel depth for all
+# images
+def moreRed(percMore):
   pic = get_pic()
   pixels = getPixels(pic)
-  percentage = input("From 0-100, what percentage do you wish to increase the red pixel value: ")
-  p = 1+(percentage/100)
-  for x in pixels:
-    r = getRed(x)
-    newRed = r*p
-    if newRed > 255:
-      setRed(x, 255)
+  newPercTot = 1 + (percMore / 100.0)
+  for eachPix in pixels:
+    currPixRed = getRed(eachPix)
+    newRedVal = floor(currPixRed * newPercTot)
+    if (newRedVal > 255):
+      setRed(eachPix, 255)
     else:
-      setRed(x, newRed)
+      setRed(eachPix, newRedVal)
   repaint(pic)
     
 # Problem 3
@@ -76,10 +81,10 @@ def lightenUp():
   pic = get_pic()
   pixels = getPixels(pic)
   #lighten each pixel
-  for x in pixels:
-    oldColor = getColor(x)
+  for eachPix in pixels:
+    oldColor = getColor(eachPix)
     newColor = makeLighter(oldColor)
-    setColor(x, newColor)
+    setColor(eachPix, newColor)
   # output
   repaint(pic)
   
@@ -89,13 +94,13 @@ def makeNegative():
   pic = get_pic()
   pixels = getPixels(pic)
   # invert each pixel
-  for x in pixels:
-    oppositeRed = 255 - getRed(x)
-    oppositeBlue = 255 - getBlue(x)
-    oppositeGreen = 255 - getGreen(x)
-    setRed(x, oppositeRed)
-    setBlue(x, oppositeBlue)
-    setGreen(x, oppositeGreen)
+  for eachPix in pixels:
+    oppositeRed = 255 - getRed(eachPix)
+    oppositeBlue = 255 - getBlue(eachPix)
+    oppositeGreen = 255 - getGreen(eachPix)
+    setRed(eachPix, oppositeRed)
+    setBlue(eachPix, oppositeBlue)
+    setGreen(eachPix, oppositeGreen)
   # output
   repaint(pic)
     
@@ -103,33 +108,29 @@ def makeNegative():
 def BnW():
   pic = get_pic()
   pixels = getPixels(pic)
-  
-  for x in pixels:
-    redColor = getRed(x)
-    blueColor = getBlue(x)
-    greenColor = getGreen(x)
+  for eachPix in pixels:
+    redColor = getRed(eachPix)
+    blueColor = getBlue(eachPix)
+    greenColor = getGreen(eachPix)
     luminance = (redColor+blueColor+greenColor)/3
-    setRed(x, luminance)
-    setBlue(x, luminance)
-    setGreen(x, luminance)
-  
+    setRed(eachPix, luminance)
+    setBlue(eachPix, luminance)
+    setGreen(eachPix, luminance)
   repaint(pic)
   
 def betterBnW():
   pic = get_pic()
   pixels = getPixels(pic)
-  
-  for x in pixels:
-    redColor = getRed(x)
+  for eachPix in pixels:
+    redColor = getRed(eachPix)
     redColor = redColor*0.299
-    blueColor = getBlue(x)
+    blueColor = getBlue(eachPix)
     blueColor = blueColor*0.587
-    greenColor = getGreen(x)
+    greenColor = getGreen(eachPix)
     greenColor = greenColor*0.114
     luminance = (redColor+blueColor+greenColor)/3
-    setRed(x, luminance)
-    setBlue(x, luminance)
-    setGreen(x, luminance)
-  
+    setRed(eachPix, luminance)
+    setBlue(eachPix, luminance)
+    setGreen(eachPix, luminance)
   repaint(pic)
   
